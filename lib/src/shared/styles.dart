4878f06d83a75df5a1eaa7a2891167f9640a400a
Luis Napoli <luispaulo.napoli@outlook.com>
2022-09-01 13:35:41 -0300

    add shared folder

---
+++ lib/src/shared/styles.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/rendering.dart';
+
+//w400
+const TextStyle h1 = TextStyle(
+  fontSize: 53,
+  fontWeight: FontWeight.w400,
+);
+const TextStyle h2 = TextStyle(
+  fontSize: 43,
+  fontWeight: FontWeight.w400,
+);
+const TextStyle h3 = TextStyle(
+  fontSize: 34,
+  fontWeight: FontWeight.w400,
+);
+const TextStyle h4 = TextStyle(
+  fontSize: 27,
+  fontWeight: FontWeight.w400,
+);
+const TextStyle h5 = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w400,
+);
+const TextStyle h6 = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle subTitle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
+const TextStyle iput = TextStyle(
+  fontSize: 11,
+  fontWeight: FontWeight.w400,
+);
+const TextStyle miniText = TextStyle(
+  fontSize: 9,
+  fontWeight: FontWeight.w400,
+);
+
+//Bold
+const TextStyle h4Bbold = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+const TextStyle subTitleBold = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+const TextStyle iputBold = TextStyle(
+  fontSize: 11,
+  fontWeight: FontWeight.bold,
+);
