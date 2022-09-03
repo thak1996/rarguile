895b0e91f9ef4d50fde5d6677544e074799991f2
Franklyn Viana <franklyn.vs@hotmail.com>
2022-09-03 00:39:52 -0300

    ajusted

--- lib/myApp.dart
+++ lib/myApp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:rarguile/homePage.dart';
+
+import 'src/features/splashpage/splashpage.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
