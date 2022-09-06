b8dd8b6aff87c1cd1451c87e3614e91bf5192a34
Luis Napoli <luispaulo.napoli@outlook.com>
2022-09-06 20:54:35 -0300

    funcionalidades e alterações

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:rarguile/src/design_system/molecules/ds_app_bar.dart';
-import 'package:rarguile/src/design_system/organisms/ds_card.dart';
+
+import 'package:rarguile/widgets/hide_show_widget.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appBar: DsAppBar(title: 'Teste'),
-        body: DsCard(
-            title: 'Aula - Flutter State',
-            text:
-                'asdadasdasdasdasdasdasdasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkaaaaaaaaaaaad',
-            date: '6-5-1029'),
-      ),
+          appBar: const DsAppBar(title: 'Teste'),
+          body: SingleChildScrollView(
+            child: Column(
+              children: const [
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                HideShow(),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+                Text('Texto'),
+              ],
+            ),
+          )),
     );
   }
 }
