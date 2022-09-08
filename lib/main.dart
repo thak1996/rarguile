45d908d4ce5489511ffa35978c5be309d91c9d21
Franklyn Viana <franklyn.vs@hotmail.com>
2022-09-08 00:44:16 -0300

    ajusted

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:rarguile/src/App.dart';
-import 'package:rarguile/src/App_module.dart';
+import 'package:rarguile/src/app.dart';
+import 'package:rarguile/src/app_module.dart';
 
 void main() {
   runApp(ModularApp(module: AppModule(), child: const App()));
