6cc33ad5ebf3ff5248e4ee5c4d374864a3a92bba
Claudevânio Conceição <claudevanio.conceicao@gmail.com>
2022-09-08 21:48:19 -0300

    app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:rarguile/src/features/login_page/login_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
