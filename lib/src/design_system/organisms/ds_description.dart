695abc2202db12916d5d0ea4e733ce15b7cb5234
Franklyn Viana <franklyn.vs@hotmail.com>
2022-09-09 23:10:51 -0300

    Ajusted

--- lib/src/design_system/organisms/ds_description.dart
+++ lib/src/design_system/organisms/ds_description.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:rarguile/src/design_system/atoms/ds_text.dart';
 import 'package:rarguile/src/shared/app_colors.dart';
 import 'package:rarguile/src/shared/styles.dart';
-import 'package:rarguile/widgets/video_widget.dart';
+import 'package:rarguile/src/widgets/video_widget.dart';
 
 class DsDescription extends StatefulWidget {
   const DsDescription(
