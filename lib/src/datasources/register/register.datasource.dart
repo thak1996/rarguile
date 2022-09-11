e919cfe7cb17c6f08a96f630e57a0b673364fa79
Claudevânio Conceição <claudevanio.conceicao@gmail.com>
2022-09-11 17:36:08 -0300

    registro de novo aluno implementado

--- lib/src/datasources/register/register.datasource.dart
+++ lib/src/datasources/register/register.datasource.dart
@@ -1,18 +1,28 @@
 import 'dart:io';
-
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:rarguile/features/registration_page/model/register_model.dart';
 import 'package:rarguile/src/service/api_service.dart';
 import 'package:rarguile/src/shared/failure.dart';
 
-class LoginDataSource {
+class RegisterDataSource {
   final ApiService service;
 
-  LoginDataSource(this.service);
+  RegisterDataSource(this.service);
 
-  Future<RegisterModel> register({required String email, required String password, required String name, required String acessCode}) async {
+  Future<RegisterModel> register(
+      {required String email,
+      required String password,
+      required String name,
+      required String acessCode}) async {
     try {
-      final response = await service.post(route: '/auth/cadastrar', body: {"email": email, "senha": password, "nome": name, "codigoAcesso": password});
+      final response = await service.post(route: '/auth/cadastrar', body: {
+        "email": email,
+        "senha": password,
+        "nome": name,
+        "codigoAcesso": password
+      });
       if (response.statusCode == 201) {
+        Modular.to.pushNamed('/login/');
         final user = RegisterModel.fromJson(response.body);
         return user;
       }
