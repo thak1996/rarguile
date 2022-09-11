0027909b96c37e6e8cfec2ae966dc5c3077631df
Claudevânio Conceição <claudevanio.conceicao@gmail.com>
2022-09-11 16:18:44 -0300

    try catch no user store implementado

--- lib/src/stores/user_store.dart
+++ lib/src/stores/user_store.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:mobx/mobx.dart';
 import 'package:rarguile/src/models/account_model.dart';
 import 'package:rarguile/src/service/api_service.dart';
+import 'package:rarguile/src/shared/failure.dart';
 part 'user_store.g.dart';
 
 class UserStore = _UserStoreBase with _$UserStore;
@@ -29,10 +32,25 @@ abstract class _UserStoreBase with Store {
   }
 
   @action
-  Future<void> user({required String email, required String password}) async {
-    Map<String, dynamic> body = {'email': email, 'senha': password};
-    var response = await service.post(route: 'auth/login', body: body);
-    var user = AccountModel.fromMap(response.data);
-    setCurrentUser(userModel: user);
+  Future<void> user(
+      {required String email, required String password}) async {
+    try {
+      Map<String, dynamic> body = {'email': email, 'senha': password};
+      var response = await service.post(route: 'auth/login', body: body);
+      if (response.statusCode == 200) {
+        var user = AccountModel.fromMap(response.body);
+        setCurrentUser(userModel: user);
+      } else {
+        throw HttpException(response.reasonPhrase);
+      }
+    } on SocketException {
+      throw Failure(message: 'Falha de Conexão');
+    } on HttpException {
+      throw Failure(message: 'Estamos passando por dificuldades');
+    } on FormatException {
+      throw Failure(message: 'Estamos passando por dificuldades');
+    } catch (error) {
+      throw Failure(message: 'Ocorreu um erro: ${error.toString()}');
+    }
   }
 }
