b128f811216aa80b0e25f1e7a6bd68180d3da2b5
Claudevânio Conceição <claudevanio.conceicao@gmail.com>
2022-09-13 00:15:43 -0300

    exclusão da register_confirmation_page

--- lib/src/modules/registration_page/module/registration_page_module.dart
+++ lib/src/modules/registration_page/module/registration_page_module.dart
@@ -2,7 +2,6 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:rarguile/src/modules/registration_page/controller/registration_store.dart';
 import 'package:rarguile/src/modules/registration_page/page/registration_page.dart';
 
-import '../../registration_confirm_page/page/confirm_registration_page.dart';
 
 class RegistrationPageModule extends Module {
   @override
@@ -14,7 +13,5 @@ class RegistrationPageModule extends Module {
   final List<ModularRoute> routes = [
     ChildRoute(Modular.initialRoute,
         child: (context, args) => const RegistrationPage()),
-    ChildRoute('/confirmation/',
-        child: (context, args) => const ConfirmRegistrationPage()),
   ];
 }
